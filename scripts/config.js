8472f618802e3396a416e2f718c04fb9e043794e
daifee <mr_zhangjiayong@163.com>
2022-07-15 19:29:02 +0800

    init

---
+++ scripts/config.js
@@ -0,0 +1,15 @@
+/**
+ * 配置
+ */
+const fs = require('fs');
+const path = require('path');
+
+const ARCHIVE_PATH = path.resolve('./', 'archives');
+fs.mkdirSync(ARCHIVE_PATH, {recursive: true});
+
+const SOURCE_PATH = path.resolve('./', 'source');
+fs.mkdirSync(SOURCE_PATH, {recursive: true});
+
+module.exports = {
+  ARCHIVE_PATH
+};
