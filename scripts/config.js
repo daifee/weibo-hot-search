4b313fcedc303a4272c286cc9e1d532653a095bb
daifee <mr_zhangjiayong@163.com>
2022-07-16 15:23:18 +0800

    增加 lint

--- scripts/config.js
+++ scripts/config.js
@@ -5,11 +5,12 @@ const fs = require('fs');
 const path = require('path');
 
 const ARCHIVE_PATH = path.resolve('./', 'archives');
-fs.mkdirSync(ARCHIVE_PATH, {recursive: true});
+fs.mkdirSync(ARCHIVE_PATH, { recursive: true });
 
 const SOURCE_PATH = path.resolve('./', 'source');
-fs.mkdirSync(SOURCE_PATH, {recursive: true});
+fs.mkdirSync(SOURCE_PATH, { recursive: true });
 
 module.exports = {
-  ARCHIVE_PATH
+  ARCHIVE_PATH,
+  SOURCE_PATH,
 };
