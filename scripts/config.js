ff20bdf044e92722c40dc707c1658c61fec38739
daifee <mr_zhangjiayong@163.com>
2022-07-18 10:05:07 +0800

    简化代码

--- scripts/config.js
+++ scripts/config.js
@@ -9,28 +9,35 @@ const basePath = process.env.NODE_ENV === 'test' ? './temp' : './';
 
 // 源数据目录
 const SOURCE_PATH = path.resolve(basePath, 'source');
-fs.mkdirSync(SOURCE_PATH, { recursive: true });
-
+/**
+ * 归档目录
+ */
 const ARCHIVE_PATH = path.resolve(basePath, 'archives');
-
 // 日榜归档目录
 const ARCHIVE_DAILY_PATH = path.resolve(ARCHIVE_PATH, 'daily');
-fs.mkdirSync(ARCHIVE_DAILY_PATH, { recursive: true });
-
 // 周榜归档目录
 const ARCHIVE_WEEKLY_PATH = path.resolve(ARCHIVE_PATH, 'weekly');
-fs.mkdirSync(ARCHIVE_WEEKLY_PATH, { recursive: true });
-
 // 月榜归档目录
 const ARCHIVE_MONTHLY_PATH = path.resolve(ARCHIVE_PATH, 'monthly');
-fs.mkdirSync(ARCHIVE_MONTHLY_PATH, { recursive: true });
-
 // 年榜归档目录
 const ARCHIVE_YEARLY_PATH = path.resolve(ARCHIVE_PATH, 'yearly');
-fs.mkdirSync(ARCHIVE_YEARLY_PATH, { recursive: true });
-
 // 模板目录
 const TEMPLATES_PATH = path.resolve('./', 'scripts/templates');
+
+
+// 创建目录
+[
+  SOURCE_PATH,
+  ARCHIVE_PATH,
+  ARCHIVE_DAILY_PATH,
+  ARCHIVE_WEEKLY_PATH,
+  ARCHIVE_MONTHLY_PATH,
+  ARCHIVE_YEARLY_PATH,
+  TEMPLATES_PATH
+].forEach((dir) => {
+  fs.mkdirSync(dir, { recursive: true });  
+});
+
 
 module.exports = {
   ARCHIVE_DAILY_PATH,
